281c1732cc4173b70035361a0aefe9dc247337f6
Andrei Macavei <andrei.macavei89@gmail.com>
2015-02-17 02:51:02 +0200

    Added view and template for registration

--- myqueue.py
+++ myqueue.py
@@ -96,15 +96,40 @@ def login():
 
     return render_template('login.html', error=error)
 
-@app.route('/logout')
-def logout():
-    """Logs the user out"""
-    return render_template('logout.html')
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
-    """Register the user."""
-    pass
+    """Registers the user."""
+    if g.user:
+        return redirect(url_for('homepage'))
+    error = None
+    if request.method == 'POST':
+        if not request.form['username']:
+            error = 'You have to enter a username'
+        elif not request.form['email'] or '@' not in request.form['email']:
+            error = 'You have to enter a valid email address'
+        elif not request.form['password']:
+            error = 'You have to enter a password'
+        elif request.form['password'] != request.form['password2']:
+            error = 'The two passwords do not match'
+        elif get_user_id(request.form['username']) is not None:
+            error = 'The username is already taken'
+        else:
+            db = get_db()
+            db.execute('''insert into user (
+                username, email, pw_hash) values(?, ?, ?)''',
+                [request.form['username'],request.form['email'],
+                generate_password_hash(request.form['password'])])
+            db.commit()
+            return redirect(url_for('login'))
+    return render_template('register.html', error=error)
+
+@app.route('/logout')
+def logout():
+    """Logs the user out."""
+    session.pop('user_id', None)
+    return render_template('logout.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
